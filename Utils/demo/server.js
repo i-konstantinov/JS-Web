7daec0b6c6a43974cc88883009177ff121a5807f
i-konstantinov <ivankonstantwork@gmail.com>
2022-01-16 16:16:41 +0200

    Piping

--- Utils/demo/server.js
+++ Utils/demo/server.js
@@ -1,20 +1,37 @@
 const http = require('http');
+const fs = require('fs');
 
 // създаваме сървъра, задаваме му порт
 // проверяваме типа на request метода
-// ако е POST, абонираме се за събитие, което носи данни /и логваме 'парчето' данни/
+// ако е POST, абонираме се за събитие, което носи 'парче' данни /с което можем да работим по натам/
 // данните идват като буфер, затова .toString()
 // добавяме стринга към body;
 // абонираме се и за събитие, което съобщава края на данните;
-// парсваме стринга body в обект, за да можем да го модифицираме /но за write ни трябва str/
+// парсваме стринга body в обект, за да можем да го модифицираме /но за ".write" ни трябва string/
 // задаваме вида на контента в хедърите
 // "пишем" response-a и го приключваме
 http.createServer((req, res) => {
     let body = '';
     
     if (req.method == 'GET') {
-        res.write('OK');
-        res.end();
+        fs.createReadStream('./index.html').pipe(res);
+
+        /*
+        const fileStream = fs.createReadStream('./index.html');
+        fileStream.on('data', (chunk) => {
+            res.write(chunk);
+        });
+        fileStream.on('end', () => {
+            res.end();
+        });
+        */
+        
+        /*
+        fs.readFile('./index.html', (err, data) => {
+            res.write(data.toString());
+            res.end();
+        });
+        */
     } else if (req.method == 'POST') {
         req.on('data', data => {
             console.log('Chunk >>>', data.toString());
